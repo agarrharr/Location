2f4b3bb54c7bf1ee2bf72716147c5deeb5fa9934
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2021-10-08 14:45:41 -0600

    Initial Commit

---
+++ Location/LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationApp.swift
+//  Location
+//
+//  Created by Adam Garrett-Harris on 10/8/21.
+//
+
+import SwiftUI
+
+@main
+struct LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
